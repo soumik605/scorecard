205be1ef053e54b48d8e0db0bb8e9ed04ff84984
Soumik Maity <soumik.maity@geogo.in>
2024-11-13 19:33:34 +0530

    updated

--- app/controllers/matches_controller.rb
+++ app/controllers/matches_controller.rb
@@ -1,13 +1,26 @@
 class MatchesController < ApplicationController
-  before_action :set_match, only: [:show]
+  before_action :set_match, only: [:show, :update]
 
   def show
+  end
+
+  def update
+    if @match.update(match_params)
+      respond_to do |format|
+        format.turbo_stream { redirect_to request.referrer, notice: 'Done !!'}
+      end
+    else
+    end
   end
 
   private
 
   def set_match
     @match = Match.find_by(id: params[:id])
+  end
+
+  def match_params
+    params.require(:match).permit(:winner_team_id, :date, :is_won_by_innings, :is_won_by_follow_on, :is_draw)
   end
   
 end
--- app/models/match.rb
+++ app/models/match.rb
@@ -3,5 +3,27 @@ class Match < ApplicationRecord
   belongs_to :winner_team, class_name: 'Team', optional: true # Winner team reference
   has_many :teams, dependent: :destroy
   has_many :performances, dependent: :destroy
+
+  after_update :add_points_in_teams
+
+  private 
+
+  def add_points_in_teams
+    p saved_changes
+    if saved_changes.has_key? 'winner_team_id' and self.winner_team_id.present?
+      win_point = tournament.win_point
+      draw_point = tournament.draw_point
+      innings_win_point = is_won_by_innings ? tournament.innings_win_point : 0
+      follow_on_win_point = is_won_by_follow_on ? tournament.follow_on_win_point : 0
+
+      teams.each do |t|
+        if t.id == self.winner_team_id
+          t.update(win_point: win_point, innings_win_point: innings_win_point, follow_on_win_point: follow_on_win_point, total_point: (win_point + innings_win_point + follow_on_win_point))
+        else
+          t.update(win_point: 0, innings_win_point: -innings_win_point, follow_on_win_point: -follow_on_win_point, total_point: -(innings_win_point+follow_on_win_point))
+        end
+      end
+    end
+  end
   
 end
