08e3e9d11961a6ec5259ffe4ef2af4d8c5e3885e
Soumik Maity <soumik.maity@geogo.in>
2024-11-16 18:48:42 +0530

    updated

--- app/controllers/rankings_controller.rb
+++ app/controllers/rankings_controller.rb
@@ -4,19 +4,24 @@ class RankingsController < ApplicationController
 
     @runs = []
     @players.each do |p|
-      latest_perf = Performance.where(id: p.performances.last(10).pluck(:id))
+      latest_perf = Performance.where(id: p.performances.where.not(runs: nil).last(10).pluck(:id))
       out = latest_perf.where(is_not_out: false).count
+      not_out = latest_perf.count - latest_perf.where(is_not_out: false).count
       total = latest_perf.pluck(:runs).compact.select { |x| x.is_a?(Numeric) && x != 0 }.sum
-      @runs << [p.name, total, out>0 ? total/out : "-"]
+      point = total * (1 + (not_out.to_f / latest_perf.count.to_f))
+      @runs << [p.name, total, out>0 ? total/out : "-", point]
     end
-    @runs = @runs.sort_by { |name, score| [-score, name] }
+    @runs = @runs.sort_by { |name, score, average, point| [-point, score, average, name] }
 
 
     @wickets = []
     @players.each do |p|
-      @wickets << [p.name, p.performances.last(10).pluck(:wickets).compact.select { |x| x.is_a?(Numeric) && x != 0 }.sum]
+      innings = p.performances.where.not(wickets: nil).last(10)
+      total_wickets = innings.pluck(:wickets).compact.select { |x| x.is_a?(Numeric) && x != 0 }.sum
+      point = (total_wickets.to_f / innings.count.to_f) * 100
+      @wickets << [p.name, point, total_wickets]
     end
-    @wickets = @wickets.sort_by { |name, score| [-score, name] }
+    @wickets = @wickets.sort_by { |name, point, wicket| [-point, wicket, name] }
 
 
   end
