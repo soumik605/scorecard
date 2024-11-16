bd76bd10622dafbdeb1a5bf9c502e18f2e4141ab
Soumik Maity <soumik.maity@geogo.in>
2024-11-16 17:25:25 +0530

    updated

--- app/controllers/rankings_controller.rb
+++ app/controllers/rankings_controller.rb
@@ -5,9 +5,9 @@ class RankingsController < ApplicationController
     @runs = []
     @players.each do |p|
       latest_perf = Performance.where(id: p.performances.last(10).pluck(:id))
-      not_out = latest_perf.where(is_not_out: true).count
+      out = latest_perf.where(is_not_out: false).count
       total = latest_perf.pluck(:runs).compact.select { |x| x.is_a?(Numeric) && x != 0 }.sum
-      @runs << [p.name, total, not_out>0 ? total/not_out : "-"]
+      @runs << [p.name, total, out>0 ? total/out : "-"]
     end
     @runs = @runs.sort_by { |name, score| [-score, name] }
 
