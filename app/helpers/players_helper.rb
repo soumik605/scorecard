09c716b30b9150e4594f0d8f7cd2de1fd2b53b86
Soumik Maity <soumik.maity@geogo.in>
2024-12-21 21:16:43 +0530

    updated

--- app/helpers/players_helper.rb
+++ app/helpers/players_helper.rb
@@ -1,19 +1,24 @@
 module PlayersHelper
 
-  def getPlayerRunData player, performances, matches
-    innings = performances.filter{|p| (p["player_id"] == player["id"]) && p["runs"].present? }
-    matches = matches.filter{ |m| innings.pluck("match_id").include?(m["id"])}.uniq
-    obj = innings.pluck("runs", "is_not_out").compact.select { |x| x[0].is_a?(Numeric)}
-    p obj
+  def getPlayerRunData player, performances, matches, tour_type, tournament
+    tour_ids = tournament.filter{|t| t["tour_type"] == tour_type}.pluck("id")
+    matches = matches.filter{ |m| tour_ids.include?(m["tournament_id"]) }
+
+    match_ids = matches.pluck("id")
+    player_match_ids = matches.filter{|m| [m["captain_a"], m["captain_b"]].include? player["id"]}
+    
+    performances = performances.filter{|p| p["player_id"] == player["id"] && p["runs"].present? && match_ids.include?(p["match_id"]) }
+
+    obj = performances.pluck("runs", "is_not_out").compact.select { |x| x[0].is_a?(Numeric)}
     total_run = obj.map(&:first).sum
-    not_out = innings.filter{ |i| i["is_not_out"] == true}.count
-    out = innings.filter{ |i| i["is_not_out"] == false}.count
+    not_out = performances.filter{ |i| i["is_not_out"] == true}.count
+    out = performances.filter{ |i| i["is_not_out"] == false}.count
     average = out > 0 ? total_run/out : nil
-    zero_count = innings.filter{ |i| i["runs"] == 0}.count
-    range_1_count = innings.filter{ |i| i["runs"] >= 1 && i["runs"] <= 30}.count
-    range_2_count = innings.filter{ |i| i["runs"] >= 31 && i["runs"] <= 49}.count
-    range_3_count = innings.filter{ |i| i["runs"] >= 50 && i["runs"] <= 99}.count
-    range_4_count = innings.filter{ |i| i["runs"] >= 100}.count
+    zero_count = performances.filter{ |i| i["runs"] == 0}.count
+    range_1_count = performances.filter{ |i| i["runs"] >= 1 && i["runs"] <= 30}.count
+    range_2_count = performances.filter{ |i| i["runs"] >= 31 && i["runs"] <= 49}.count
+    range_3_count = performances.filter{ |i| i["runs"] >= 50 && i["runs"] <= 99}.count
+    range_4_count = performances.filter{ |i| i["runs"] >= 100}.count
     max_value, is_not_out = nil, nil
     
     max_value, is_not_out = obj.max_by { |num, bool| [num, bool ? 1 : 0] } if obj.present? 
@@ -23,22 +28,28 @@ module PlayersHelper
       highest = is_not_out ? "#{max_value}*" : max_value
     end
 
-    return [total_run, innings.count, average, matches.count, highest, not_out, zero_count, range_1_count, range_2_count, range_3_count, range_4_count]
+    return [total_run, performances.count, average, player_match_ids.count, highest, not_out, zero_count, range_1_count, range_2_count, range_3_count, range_4_count]
   end
 
 
 
-  def getPlayerWicketData player, performances, matches
-    innings = performances.filter{|p| (p["player_id"] == player["id"]) && p["wickets"].present? }
-    matches = matches.filter{ |m| innings.pluck("match_id").include?(m["id"])}.uniq
-    all_wickets = innings.pluck("wickets").compact.select { |x| x.is_a?(Numeric) }
+  def getPlayerWicketData player, performances, matches, tour_type, tournament
+    tour_ids = tournament.filter{|t| t["tour_type"] == tour_type}.pluck("id")
+    matches = matches.filter{ |m| tour_ids.include?(m["tournament_id"]) }
+
+    match_ids = matches.pluck("id")
+    player_match_ids = matches.filter{|m| [m["captain_a"], m["captain_b"]].include? player["id"]}
+    
+    performances = performances.filter{|p| p["player_id"] == player["id"] && p["runs"].present? && match_ids.include?(p["match_id"]) }
+
+    all_wickets = performances.pluck("wickets").compact.select { |x| x.is_a?(Numeric) }
     total_wickets = all_wickets.sum
     zero_count = all_wickets.count(0)
     range_1_count = all_wickets.count(3)
     range_2_count = all_wickets.count(4)
     range_3_count = all_wickets.count { |num| num > 4 }
 
-    return [total_wickets, matches.count, innings.count, range_1_count, range_2_count, range_3_count, zero_count]
+    return [total_wickets, player_match_ids.count, performances.count, range_1_count, range_2_count, range_3_count, zero_count]
   end
 
 
