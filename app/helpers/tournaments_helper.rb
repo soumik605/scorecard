3d70f2fbe56634c63e59f4a4b350b1e92f107b20
Soumik Maity <soumik.maity@geogo.in>
2024-12-30 11:43:15 +0530

    updated

--- app/helpers/tournaments_helper.rb
+++ app/helpers/tournaments_helper.rb
@@ -1,2 +1,16 @@
 module TournamentsHelper
+
+  def getResultClass cap_a_id, cap_b_id, win_cap_id
+    cls = ""
+    if win_cap_id.present?
+      if cap_a_id == win_cap_id
+        cls = "bg-gradient-to-r from-green-500 via-white to-red-500"
+      elsif cap_b_id == win_cap_id
+        cls = "bg-gradient-to-r from-red-500 via-white to-green-500"
+      end
+    end
+
+    return cls
+  end
+
 end
