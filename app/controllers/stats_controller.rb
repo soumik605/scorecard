b6c7e2155f2a412b13f69a3907519d9f7f8980b9
Soumik Maity <soumik.maity@geogo.in>
2025-02-10 09:43:38 +0530

    Add playoff performance statistics for players and captains

--- app/controllers/stats_controller.rb
+++ app/controllers/stats_controller.rb
@@ -14,17 +14,24 @@ class StatsController < ApplicationController
     @tours = @tours.filter{|t| tour_type == t["tour_type"]}
     @matches = @matches.filter{|m| @tours.pluck("id").include?(m["tournament_id"])}
     @performances = @performances.filter{|p| @matches.pluck("id").include?(p["match_id"])}
+    
+    @playoff_matches = @matches.filter{|m| ( @tours.pluck("id").include?(m["tournament_id"]) && ["q1", "q2", "final"].include?(m["match_type"]) )}
+    @playoff_performances = @performances.filter{|p| @playoff_matches.pluck("id").include?(p["match_id"])}
 
     @captain_stats = []
     @player_stats = []
 
     @player_stats << ["Most run", Player.most_runs(@performances, @players)]
     @player_stats << ["Most wicket", Player.most_wickets(@performances, @players)]
+    @player_stats << ["Most run in knock-out", Player.most_runs(@playoff_performances, @players)]
+    @player_stats << ["Most wicket in knock-out", Player.most_wickets(@playoff_performances, @players)]
     @player_stats << ["Most duck", Player.most_ducks(@performances, @players)]
     @player_stats << ["Most 50", Player.most_fifties(@performances, @players)]
     @player_stats << ["Most 100", Player.most_hundreds(@performances, @players)]
     @player_stats << ["Best batting innings", Performance.best_batting_innings(@performances, @players)]
     @player_stats << ["Best bowling innings", Performance.best_bowling_innings(@performances, @players)]
+    @player_stats << ["Best batting innings in knock-out", Performance.best_batting_innings(@playoff_performances, @players)]
+    @player_stats << ["Best bowling innings in knock-out", Performance.best_bowling_innings(@playoff_performances, @players)]
     @player_stats << ["Most run as player", Player.most_run_as_player(@performances, @matches, @players, false)]
     @player_stats << ["Most wicket as player", Player.most_wicket_as_player(@performances, @matches, @players, false)]
     @player_stats << ["Most consicutive innings without duck", Player.most_consicutive_innings_without_duck(@performances, @players)]
@@ -37,6 +44,8 @@ class StatsController < ApplicationController
     @captain_stats << ["Most win by follow on", Match.most_wins_by_follow_on(@matches, @players)] if tour_type == "test"
     @captain_stats << ["Most run as captain", Player.most_run_as_player(@performances, @matches, @players, true)]
     @captain_stats << ["Most wicket as captain", Player.most_wicket_as_player(@performances, @matches, @players, true)]
+    @captain_stats << ["Most run as captain in knock-out", Player.most_run_as_player(@playoff_performances, @matches, @players, true)]
+    @captain_stats << ["Most wicket as captain in knock-out", Player.most_wicket_as_player(@playoff_performances, @matches, @players, true)]
     @captain_stats << ["Most consicutive win", Player.most_consicutive_win(@matches, @players)]
     @captain_stats << ["Best win % against a captain (min 5 match)", Match.best_win_percent_against_captain(@matches, @players)]
 
