2e22c3efb8518ad8a4c376f323831a281a11be1e
Soumik Maity <soumik.maity@geogo.in>
2025-03-06 15:21:22 +0530

    Refactor tournament leaderboard data structure and display format

--- app/controllers/tournaments_controller.rb
+++ app/controllers/tournaments_controller.rb
@@ -51,11 +51,16 @@ class TournamentsController < ApplicationController
       win_point = win_matches.pluck("win_point").compact.select { |x| x.is_a?(Numeric) && x != 0 }.sum
       loose_point = loose_matches.pluck("loose_point").compact.select { |x| x.is_a?(Numeric) && x != 0 }.sum
       win_percent = player_matches.present? ? (win_matches.count.to_f / player_matches.count.to_f) * 100 : 0
-      win_percent = win_percent.try(:round)
-      @players_data << [player["photo_name"], player_matches.filter{|m| m["winner_captain_id"].present?}.count, player_matches.filter{|m| !m["winner_captain_id"].present?}.count, win_point+loose_point, "#{win_percent}%"]
+      
+      win_percent = "#{win_percent.try(:round)}%"
+      win_match_count = win_matches.count
+      total_match_count = player_matches.count
+      total_point = win_point+loose_point
+
+      @players_data << [player["photo_name"], total_point, win_match_count, total_match_count, win_percent ]
     end
 
-    @players_data = @players_data.sort_by { |photo_name, complete, pending, score| [-score, complete, -pending, photo_name] }
+    @players_data = @players_data.sort_by { |photo_name, point, win, total, per| [-point, total, photo_name] }
 
   end
 
