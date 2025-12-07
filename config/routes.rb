2652c4aa85788f5975271f9742c5ed158370f507
Soumik Maity <soumik.maity@geogo.in>
2025-12-07 21:54:48 +0530

    updated

--- config/routes.rb
+++ config/routes.rb
@@ -72,6 +72,7 @@ Rails.application.routes.draw do
   get  '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
   get '/logout', to: 'sessions#destroy'
+  get '/users', to: 'users#index'
 
 
 end
