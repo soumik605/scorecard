f11d2e79a41621b48c9ce64eba7af3a9213efc29
Soumik Maity <soumik.maity@geogo.in>
2025-12-15 11:41:28 +0530

    updated

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -60,7 +60,7 @@ class RoomsController < ApplicationController
   end
 
     def players
-      @users = User.all
+      @users = User.all.where(room_id: params[:id])
       p @users
       @players = PickedPlayer.where(room_id: params[:id]).where.not(user_id: nil)
     end
