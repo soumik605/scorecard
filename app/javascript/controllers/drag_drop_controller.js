00475d5a883fe6a6d7bc730a6ce87a3357ad79b4
Soumik Maity <soumik.maity@geogo.in>
2025-12-15 19:10:39 +0530

    updated

--- app/javascript/controllers/drag_drop_controller.js
+++ app/javascript/controllers/drag_drop_controller.js
@@ -1,9 +1,16 @@
-// app/javascript/controllers/drag_drop_controller.js
 import { Controller } from "@hotwired/stimulus";
 
 export default class extends Controller {
+  connect() {
+    this.draggedPlayerId = null;
+  }
+
   drag(event) {
-    event.dataTransfer.setData("player_id", event.target.dataset.id);
+    this.draggedPlayerId = event.target.dataset.playerId;
+  }
+
+  touchStart(event) {
+    this.draggedPlayerId = event.target.dataset.playerId;
   }
 
   allow(event) {
@@ -13,21 +20,24 @@ export default class extends Controller {
   drop(event) {
     event.preventDefault();
 
-    const playerId = event.dataTransfer.getData("player_id");
+    const playerId = this.draggedPlayerId;
     const teamType = event.currentTarget.dataset.teamType;
+
+    if (!playerId) {
+      alert("Unable to detect player. Please tap again.");
+      return;
+    }
 
     fetch(`/rooms/${playerId}/update_team_type`, {
       method: "PATCH",
       headers: {
-        "X-CSRF-Token": document.querySelector("meta[name='csrf-token']")
-          .content,
+        "X-CSRF-Token": document.querySelector("meta[name='csrf-token']").content,
         "Content-Type": "application/json",
         Accept: "text/vnd.turbo-stream.html, text/html",
       },
       body: JSON.stringify({ team_type: teamType }),
     }).then((response) => {
       if (response.redirected) {
-        // Rails redirect → follow it
         window.location.href = response.url;
       } else if (!response.ok) {
         alert("Max 11 players allowed.");
